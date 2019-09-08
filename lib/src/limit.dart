793e2a51da032c3aba3c7ca6ef5be8840ac03429
ookami.kb <ookami.kb@gmail.com>
2019-09-08 15:00:55 +0200

    :tv: Update limit func doc

--- lib/src/limit.dart
+++ lib/src/limit.dart
@@ -1,8 +1,14 @@
 import 'package:dfunc/dfunc.dart';
 
-/// Returns string limited to the `maxLength` number of characters.
+/// Returns functions that limits passed string to the `maxLength`
+/// number of characters.
 ///
-/// If passed string is shorter than limit, it returns passed string.
-/// If passed string is `null`, it returns empty string.
+/// If passed string is shorter than limit, it will return passed string.
+/// If passed string is `null`, it will return empty string.
+///
+/// ```dart
+/// final limit3 = limit(3);
+/// limit3('Test') // 'Tes'
+/// ```
 String Function(String) limit(int maxLength) =>
     (String s) => substring(s, 0, maxLength);
