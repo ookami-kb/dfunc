867316633fd6cec03ef909ccee4a513af3cd8c9d
ookami.kb <ookami.kb@gmail.com>
2020-04-12 14:37:34 +0200

    :cop: Refactor unions to have const constructors

--- lib/src/coproduct/coproduct.dart
+++ lib/src/coproduct/coproduct.dart
@@ -1,14 +1,22 @@
-abstract class Coproduct0 {}
+abstract class Coproduct0 {
+  const Coproduct0();
+}
 
 abstract class Coproduct1<T1> {
+  const Coproduct1();
+
   R match<R>(R Function(T1) ifFirst);
 }
 
 abstract class Coproduct2<T1, T2> {
+  const Coproduct2();
+
   R match<R>(R Function(T1) ifFirst, R Function(T2) ifSecond);
 }
 
 abstract class Coproduct3<T1, T2, T3> {
+  const Coproduct3();
+
   R match<R>(
     R Function(T1) ifFirst,
     R Function(T2) ifSecond,
@@ -17,6 +25,8 @@ abstract class Coproduct3<T1, T2, T3> {
 }
 
 abstract class Coproduct4<T1, T2, T3, T4> {
+  const Coproduct4();
+
   R match<R>(
     R Function(T1) ifFirst,
     R Function(T2) ifSecond,
@@ -26,6 +36,8 @@ abstract class Coproduct4<T1, T2, T3, T4> {
 }
 
 abstract class Coproduct5<T1, T2, T3, T4, T5> {
+  const Coproduct5();
+
   R match<R>(
     R Function(T1) ifFirst,
     R Function(T2) ifSecond,
