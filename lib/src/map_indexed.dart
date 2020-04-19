465423c528c6d5f85531c504163b10d4b3d5a9d6
ookami.kb <ookami.kb@gmail.com>
2020-04-19 12:37:36 +0200

    :zap: Add mapIndexed extension function

--- lib/src/map_indexed.dart
+++ lib/src/map_indexed.dart
@@ -1,5 +1,18 @@
 /// Returns a new [Iterable] with elements that are created by
-///  calling `f` on each element of this `List` in iteration order and
+///  calling `f` on each element of [items] in iteration order and
 ///  provides position of the element as a first argument of `f`.
-Iterable<T> mapIndexed<T, E>(T f(int i, E e), List<E> items) =>
-    items.asMap().map((int i, E e) => MapEntry(i, f(i, e))).values;
+Iterable<T> mapIndexed<T, E>(T f(int i, E e), Iterable<E> items) =>
+    _mapIndexed(f, items);
+
+extension MapIndexed<E> on Iterable<E> {
+  /// Returns a new [Iterable] with elements that are created by
+  ///  calling `f` on each element of current iterable in iteration order and
+  ///  provides position of the element as a first argument of `f`.
+  Iterable<T> mapIndexed<T>(T Function(int i, E element) f) =>
+      _mapIndexed(f, this);
+}
+
+Iterable<T> _mapIndexed<T, E>(T f(int i, E e), Iterable<E> items) {
+  final list = items is List<E> ? items : items.toList();
+  return list.asMap().map((int i, E e) => MapEntry(i, f(i, e))).values;
+}
--- test/map_indexed_test.dart
+++ test/map_indexed_test.dart
@@ -6,4 +6,9 @@ void main() {
     final items = ['a', 'b', 'c'];
     expect(mapIndexed((i, e) => '$e$i', items), ['a0', 'b1', 'c2']);
   });
+
+  test('extension maps elements with index', () {
+    final items = ['a', 'b', 'c'];
+    expect(items.mapIndexed((i, e) => '$e$i'), ['a0', 'b1', 'c2']);
+  });
 }
