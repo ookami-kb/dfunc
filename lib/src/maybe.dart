5978833dd6b15f00d6f6c22e9ea5a761c6e13f65
Kirill Bubochkin <ookami.kb@gmail.com>
2023-04-08 18:36:38 +0200

    feat: add `maybeWhereType` (#22)
    Fixes #21

--- lib/src/maybe.dart
+++ lib/src/maybe.dart
@@ -22,6 +22,20 @@ extension Maybe<A extends Object> on A? {
   /// ```
   A? maybeWhere(Predicate<A> f) => maybeFlatMap((v) => f(v) ? v : null);
 
+  /// Filters value based on type [B] and returns either value itself or null.
+  ///
+  /// Same as you can filter the list with `whereType` method to remove
+  /// undesired types (and probably get an empty list), you can use
+  /// `maybeWhereType` to "filter" by type. As a result you will get either the
+  /// value itself, or null if its type is not [B]:
+  ///
+  /// ```dart
+  /// final int b = 1;
+  /// b.maybeWhereType<int>(); // 1
+  /// b.maybeWhereType<String>(); // null
+  /// ```
+  B? maybeWhereType<B>() => this?.let((it) => it is B ? it as B : null);
+
   /// Returns result of [f] if `this` is null, else returns `this`.
   A ifNull(A Function() f) => this ?? f();
 }
--- test/maybe_test.dart
+++ test/maybe_test.dart
@@ -4,63 +4,106 @@ import 'package:dfunc/dfunc.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('maybeMap for null', () {
-    const String? x = null;
-    expect(x.maybeMap((e) => e.toUpperCase()), null);
+  group(
+    'maybeMap:',
+    () {
+      test('for null returns null', () {
+        const String? x = null;
+        expect(x.maybeMap((e) => e.toUpperCase()), null);
+      });
+
+      test('for some returns some', () {
+        const String? x = 'a';
+        expect(x.maybeMap((e) => e.toUpperCase()), 'A');
+      });
+    },
+  );
+
+  group('maybeFlatMap:', () {
+    test('for null returning null', () {
+      const String? x = null;
+      expect(x.maybeFlatMap<String>((_) => null), null);
+    });
+
+    test('for null returning some', () {
+      const String? x = null;
+      expect(x.maybeFlatMap((e) => e.toUpperCase()), null);
+    });
+
+    test('for some returning null', () {
+      const String? x = 'a';
+      expect(x.maybeFlatMap<String>((_) => null), null);
+    });
+
+    test('for some returning some', () {
+      const String? x = 'a';
+      expect(x.maybeFlatMap((e) => e.toUpperCase()), 'A');
+    });
   });
 
-  test('maybeMap for some', () {
-    const String? x = 'a';
-    expect(x.maybeMap((e) => e.toUpperCase()), 'A');
+  group('maybeWhere:', () {
+    test('for some with false predicate', () {
+      const String? x = 'a';
+      expect(x.maybeWhere((e) => e.isEmpty), null);
+    });
+
+    test('for some with true predicate', () {
+      const String? x = 'a';
+      expect(x.maybeWhere((e) => e.isNotEmpty), 'a');
+    });
+
+    test('for non-null with false predicate', () {
+      const String x = 'a';
+      expect(x.maybeWhere((e) => e.isEmpty), null);
+    });
+
+    test('for non-null with true predicate', () {
+      const String x = 'a';
+      expect(x.maybeWhere((e) => e.isNotEmpty), 'a');
+    });
   });
 
-  test('maybeFlatMap for null returning null', () {
-    const String? x = null;
-    expect(x.maybeFlatMap<String>((_) => null), null);
+  group('maybeWhereType:', () {
+    test('for some with wrong type', () {
+      const String? x = 'a';
+      expect(x.maybeWhereType<int>(), null);
+    });
+
+    test('for some with right type', () {
+      const String? x = 'a';
+      expect(x.maybeWhereType<String>(), 'a');
+    });
+
+    test('for null with wrong type', () {
+      const String? x = null;
+      expect(x.maybeWhereType<int>(), null);
+    });
+
+    test('for null with right type', () {
+      const String? x = null;
+      expect(x.maybeWhereType<String>(), null);
+    });
+
+    test('for non-null with wrong type', () {
+      const String x = 'a';
+      expect(x.maybeWhereType<int>(), null);
+    });
+
+    test('for non-null with right type', () {
+      const String x = 'a';
+      expect(x.maybeWhereType<String>(), 'a');
+    });
   });
 
-  test('maybeFlatMap for null returning some', () {
-    const String? x = null;
-    expect(x.maybeFlatMap((e) => e.toUpperCase()), null);
-  });
+  group('ifNull:', () {
+    test('calls function for null value', () {
+      const String? x = null;
+      expect(x.ifNull(() => 'fallback'), 'fallback');
+    });
 
-  test('maybeFlatMap for some returning null', () {
-    const String? x = 'a';
-    expect(x.maybeFlatMap<String>((_) => null), null);
-  });
-
-  test('maybeFlatMap for some returning some', () {
-    const String? x = 'a';
-    expect(x.maybeFlatMap((e) => e.toUpperCase()), 'A');
-  });
-
-  test('maybeWhere for some with false predicate', () {
-    const String? x = 'a';
-    expect(x.maybeWhere((e) => e.isEmpty), null);
-  });
-
-  test('maybeWhere for some with true predicate', () {
-    const String? x = 'a';
-    expect(x.maybeWhere((e) => e.isNotEmpty), 'a');
-  });
-
-  test('maybeWhere for non-null with false predicate', () {
-    const String x = 'a';
-    expect(x.maybeWhere((e) => e.isEmpty), null);
-  });
-
-  test('maybeWhere for non-null with true predicate', () {
-    const String x = 'a';
-    expect(x.maybeWhere((e) => e.isNotEmpty), 'a');
-  });
-
-  test('ifNull calls function for null value', () {
-    const String? x = null;
-    expect(x.ifNull(() => 'fallback'), 'fallback');
-  });
-
-  test('ifNull does not call function for non-null value', () {
-    const String? x = 'value';
-    expect(x.ifNull(() => 'fallback'), 'value');
+    test('does not call function for non-null value', () {
+      const String? x = 'value';
+      expect(x.ifNull(() => 'fallback'), 'value');
+    });
   });
 }
