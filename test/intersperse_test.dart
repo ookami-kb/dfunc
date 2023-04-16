1f93002b180e7b948ea69cfc65ab51070bbf1cdc
ookami-kb <ookami.kb@gmail.com>
2023-04-16 14:57:13 +0200

    feat: add `beforeFirst` and `afterLast` to `intersperse(With)`

--- test/intersperse_test.dart
+++ test/intersperse_test.dart
@@ -7,6 +7,13 @@ void main() {
       expect(<int>[].intersperse(0), <int>[]);
     });
 
+    test('for empty list with beforeFirst and afterLast returns empty', () {
+      expect(
+        <int>[].intersperse(0, beforeFirst: true, afterLast: true),
+        <int>[],
+      );
+    });
+
     test('for single-item list does not add anything', () {
       expect(<int>[1].intersperse(0), <int>[1]);
     });
@@ -18,19 +25,63 @@ void main() {
     test('for 3-items list adds 2 elements', () {
       expect(<int>[1, 2, 3].intersperse(0), <int>[1, 0, 2, 0, 3]);
     });
+
+    test('for single-item list with beforeFirst adds one before', () {
+      expect(<int>[1].intersperse(0, beforeFirst: true), <int>[0, 1]);
+    });
+
+    test('for single-item list with afterLast adds one after', () {
+      expect(<int>[1].intersperse(0, afterLast: true), <int>[1, 0]);
+    });
+
+    test('for single-item list with beforeFirst and afterLast', () {
+      expect(
+        <int>[1].intersperse(0, beforeFirst: true, afterLast: true),
+        <int>[0, 1, 0],
+      );
+    });
+
+    test('for 2-items list with beforeFirst', () {
+      expect(<int>[1, 2].intersperse(0, beforeFirst: true), <int>[0, 1, 0, 2]);
+    });
+
+    test('for 2-items list with afterLast', () {
+      expect(<int>[1, 2].intersperse(0, afterLast: true), <int>[1, 0, 2, 0]);
+    });
+
+    test('for 2-items list with beforeFirst and afterLast', () {
+      expect(
+        <int>[1, 2].intersperse(0, beforeFirst: true, afterLast: true),
+        <int>[0, 1, 0, 2, 0],
+      );
+    });
   });
 
   group('intersperseWith', () {
     String buildItem(int a) => a.toString();
     String buildSeparator(int a, int b) => '<$a-$b>';
 
-    sut(Iterable<int> items) => items.intersperseWith(
+    sut(
+      Iterable<int> items, {
+      bool beforeFirst = false,
+      bool afterLast = false,
+    }) =>
+        items.intersperseWith(
           itemBuilder: buildItem,
           separatorBuilder: buildSeparator,
+          beforeFirst: beforeFirst ? (item) => '<<$item' : null,
+          afterLast: afterLast ? (item) => '$item>>' : null,
         );
 
     test('for empty list returns empty', () {
       expect(sut(<int>[]), <int>[]);
+    });
+
+    test('for empty list with beforeFirst and afterLast returns empty', () {
+      expect(
+        sut(<int>[], beforeFirst: true, afterLast: true),
+        <int>[],
+      );
     });
 
     test('for single-item list does not add anything', () {
@@ -43,6 +94,36 @@ void main() {
 
     test('for 3-items list adds 2 elements', () {
       expect(sut([1, 2, 3]), ['1', '<1-2>', '2', '<2-3>', '3']);
+    });
+
+    test('for single-item list with beforeFirst adds one before', () {
+      expect(sut([1], beforeFirst: true), ['<<1', '1']);
+    });
+
+    test('for single-item list with afterLast adds one after', () {
+      expect(sut([1], afterLast: true), ['1', '1>>']);
+    });
+
+    test('for single-item list with beforeFirst and afterLast', () {
+      expect(
+        sut([1], beforeFirst: true, afterLast: true),
+        ['<<1', '1', '1>>'],
+      );
+    });
+
+    test('for 2-items list with beforeFirst', () {
+      expect(sut([1, 2], beforeFirst: true), ['<<1', '1', '<1-2>', '2']);
+    });
+
+    test('for 2-items list with afterLast', () {
+      expect(sut([1, 2], afterLast: true), ['1', '<1-2>', '2', '2>>']);
+    });
+
+    test('for 2-items list with beforeFirst and afterLast', () {
+      expect(
+        sut([1, 2], beforeFirst: true, afterLast: true),
+        ['<<1', '1', '<1-2>', '2', '2>>'],
+      );
     });
   });
 }
